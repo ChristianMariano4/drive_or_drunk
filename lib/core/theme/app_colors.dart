1220d40d979160da05f48996f42d7412ca489f3e
Christian Mariano <160615538+ChristianMariano4@users.noreply.github.com>
2025-04-16 11:46:25 +0200

    fix: update settings.gradle (#18)

--- lib/core/theme/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color yellow = Color(0xFFFFC953);
-  static const Color white = Color(0xFFFFFFFF);
-  static const Color grey = Color(0x1C06416E);
-  static const Color blue = Color(0xFF007BFF);
-}
