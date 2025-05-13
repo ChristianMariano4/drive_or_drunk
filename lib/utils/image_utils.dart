e0e74528562b2d5d8a898082dc20a7ab85413aeb
Armando <armifio22@gmail.com>
2025-05-13 19:15:20 +0200

    Change in image_utils

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -2,8 +2,7 @@ import 'dart:convert' show base64Decode, base64Encode;
 import 'dart:io' show File;
 import 'dart:typed_data' show Uint8List;
 
-import 'package:flutter/material.dart' show Image;
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart' show AssetImage, Image, ImageProvider, MemoryImage;
 
 Image imageFromBase64(String base64String) {
   try {
@@ -11,15 +10,16 @@ Image imageFromBase64(String base64String) {
     final Uint8List bytes = base64Decode(base64Data);
     return Image.memory(bytes);
   } catch (e) {
-    return Image.asset('assets/logos/logo_android12.png');
+    return Image.asset('assets/lgoos/logo_android12.png');
   }
 }
+
 
 ImageProvider imageProviderFromBase64(String base64String) {
   try {
     final base64Data = base64String.split(',').last;
     final Uint8List bytes = base64Decode(base64Data);
-    return MemoryImage(bytes);
+    return  MemoryImage(bytes);
   } catch (e) {
     return const AssetImage('assets/logos/logo_android12.png');
   }
