5b5493742556f8348f576abedb2bd3067cb7ecac
Armando <armifio22@gmail.com>
2025-06-23 16:05:42 +0200

    Ride model implemented from already existing booking model

--- lib/models/conversation_model.dart
+++ lib/models/conversation_model.dart
@@ -45,7 +45,10 @@ class Message {
 }
 
 Future<void> setMessageSeen(String messageId, FirebaseFirestore db) async {
-  await db.collection('Messages').doc(messageId).update({'seen': true});
+  await db
+      .collection(Collections.messages)
+      .doc(messageId)
+      .update({'seen': true});
 }
 
 class Conversation {
@@ -189,7 +192,8 @@ Future<void> deleteConversation(String id, FirebaseFirestore db) async {
 Future<void> sendMessage(
     String conversationId, Message message, FirebaseFirestore db) async {
   // Add the message to the 'messages' collection and get its reference
-  final messageRef = await db.collection("Messages").add(message.toMap());
+  final messageRef =
+      await db.collection(Collections.messages).add(message.toMap());
 
   // Get the conversation
   final conversation = await getConversation(conversationId, db);
