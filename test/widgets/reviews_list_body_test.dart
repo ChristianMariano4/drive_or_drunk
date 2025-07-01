3c9be2bd1d14e38ad4c4ba2c66190d7801b1c821
Christian Mariano <160615538+ChristianMariano4@users.noreply.github.com>
2025-07-01 18:45:05 +0200

    Fix widget tests with Firebase initialization

--- test/widgets/reviews_list_body_test.dart
+++ test/widgets/reviews_list_body_test.dart
@@ -3,6 +3,8 @@ import 'package:drive_or_drunk_app/models/review_model.dart';
 import 'package:drive_or_drunk_app/models/user_model.dart' as user_model;
 import 'package:drive_or_drunk_app/services/firestore_service.dart';
 import 'package:drive_or_drunk_app/widgets/reviews_list_body.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -17,6 +19,11 @@ class FakeFirestoreService extends FirestoreService {
 }
 
 void main() {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    setupFirebaseCoreMocks();
+    await Firebase.initializeApp();
+  });
   testWidgets('ReviewsListBody displays review and author', (tester) async {
     final author = user_model.User(id: 'u', name: 'Bob', username: 'b', email: 'b@a');
     final service = FakeFirestoreService(author);
